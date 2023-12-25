bfe374eb4997fe97a3c8bbf7c1e5468041891fc9
jean luc kawel <20ka208@esisalama.org>
2023-12-25 08:01:01 +0200

    generate migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+//         \App\Models\User::factory(10)->create();
+//
+//         \App\Models\User::factory()->create([
+//             'name' => 'Test User',
+//             'email' => 'test@example.com',
+//         ]);
         Product::factory()->count(100)->create();
         User::factory()->count(10)->create();
     }
