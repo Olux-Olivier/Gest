783d0d6050c292eb0d4fa352f2082c78d3e102fd
jean luc kawel <20ka208@esisalama.org>
2023-12-30 22:53:41 +0200

    seeder

---
+++ database/factories/Order_itemFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order_item>
+ */
+class Order_itemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'order_id' => $this->faker->numberBetween(1, 10),
+            'product_id' => $this->faker->numberBetween(1, 10),
+            'quantity' => $this->faker->numberBetween(1, 10),
+            'sub_total' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
