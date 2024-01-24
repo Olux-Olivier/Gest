414811e004b78071819765afdf64f63fec9f8301
jean luc kawel <20ka208@esisalama.org>
2024-01-24 22:57:08 +0200

    rectafor route ligne 19

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('store',[\App\Http\Controllers\ProductController::class,'store']);
 
 Route::get('/article',[ProductController::class,'article']);
-Route::get('product',[\App\Http\Controllers\ProductController::class, 'product'])->name('product');
+Route::get('product',[ProductController::class, 'product'])->name('product');
 
 Route::get('/register', [\App\Http\Controllers\ProductController::class,'register']);
 Route::post('/register', [\App\Http\Controllers\ProductController::class,'createProduct']);
