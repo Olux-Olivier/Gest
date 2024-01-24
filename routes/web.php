d2a370a0634e03addb866b73a43cb4433734eec7
jean luc kawel <20ka208@esisalama.org>
2024-01-24 22:53:44 +0200

    // route store

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('store',[\App\Http\Controllers\ProductController::class,'store']);
+//Route::get('store',[\App\Http\Controllers\ProductController::class,'store']);
 
 Route::get('/article',[\App\Http\Controllers\ProductController::class,'article']);
 Route::get('product',[\App\Http\Controllers\ProductController::class, 'product'])->name('product');
