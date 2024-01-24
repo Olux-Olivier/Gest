617fd06a58482f9852e4759268b74a8378d48d10
jean luc kawel <20ka208@esisalama.org>
2024-01-24 22:56:37 +0200

    rectafor route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 //Route::get('store',[\App\Http\Controllers\ProductController::class,'store']);
 
-Route::get('/article',[\App\Http\Controllers\ProductController::class,'article']);
+Route::get('/article',[ProductController::class,'article']);
 Route::get('product',[\App\Http\Controllers\ProductController::class, 'product'])->name('product');
 
 Route::get('/register', [\App\Http\Controllers\ProductController::class,'register']);
