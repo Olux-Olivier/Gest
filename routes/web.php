de9e9d701418f8560fd282a2e35700fd28663727
jean luc kawel <20ka208@esisalama.org>
2024-02-03 10:02:36 +0200

    add view register product

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('',function (){
+    view('pages.product');
+});
+
 //Route::get('store',[\App\Http\Controllers\ProductController::class,'store']);
 
 Route::get('/article',[ProductController::class,'article']);
